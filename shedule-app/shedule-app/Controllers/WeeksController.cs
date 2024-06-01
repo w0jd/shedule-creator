fbcc71dea0fa915951b61066d65274e412cabd44
w0jd <dwoitek1@gmail.com>
2024-06-01 21:40:01 +0200

    poprawka przełączania tygonia

--- shedule-app/shedule-app/Controllers/WeeksController.cs
+++ shedule-app/shedule-app/Controllers/WeeksController.cs
@@ -28,19 +28,24 @@ namespace shedule_app.Controllers
             string StrDate = Request.Cookies["beginingOfCurrentWeek"];
             DateOnly weekBeginning=DateOnly.Parse(StrDate);
             DateOnly weekEnd = weekBeginning.AddDays(7);
-            var week = _context.Tasks.Include(a => a.Users).Where(a => a.IdUser == accountID.IdUser && a.date >= weekBeginning && a.date <= weekEnd);
+            Response.Cookies.Delete("beginingOfCurrentWeek");
+            Response.Cookies.Append("beginingOfCurrentWeek",weekEnd.AddDays(1).ToString() );
+            var week = _context.Tasks.Include(a => a.Users).Where(a => a.IdUser == accountID.IdUser && a.date >= weekBeginning && a.date < weekEnd);
             return new JsonResult(Ok(week));
           
         }
+   
         [HttpGet]
         public JsonResult PrevWeek()
         {
             var userName = User?.FindFirst(ClaimTypes.Name).Value;
             var accountID = _context.Users.First(a => a.UserName == userName);
             string StrDate = Request.Cookies["beginingOfCurrentWeek"];
-            DateOnly weekBeginning = DateOnly.Parse(StrDate);
-            DateOnly weekEnd = weekBeginning.AddDays(-7);
-            var week = _context.Tasks.Include(a => a.Users).Where(a => a.IdUser == accountID.IdUser && a.date >= weekBeginning && a.date <= weekEnd);
+            DateOnly weekEnding = DateOnly.Parse(StrDate);
+            DateOnly prevWeekBeginning= weekEnding.AddDays(-7);
+            Response.Cookies.Delete("beginingOfCurrentWeek");
+            Response.Cookies.Append("beginingOfCurrentWeek", weekEnding.AddDays(-1).ToString());
+            var week = _context.Tasks.Include(a => a.Users).Where(a => a.IdUser == accountID.IdUser && a.date >= prevWeekBeginning&& a.date < weekEnding);
             return new JsonResult(Ok(week));
 
         }
